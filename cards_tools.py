ad84cfbbd6d9fa4bc02681e8c01d920666f95dea
Hedwig <2277165192@qq.com>
2022-04-28 10:31:10 +0800

    null

--- cards_tools.py
+++ cards_tools.py
@@ -1,6 +1,3 @@
-import re
-
-
 def checkOne(stu):
     """
     This function was used to query one.If wasn't find a data you need, it will print "未查询到相关数据"
@@ -13,7 +10,16 @@ def checkOne(stu):
     for s in stu:
         if s["name"] == name:
             print("姓名:%s  电话:%s  QQ:%s  邮箱:%s" % (s["name"], s["phone"], s["qq"], s["email"]), end="\n")
-            return
+            while 1:
+                print("*" * 50, end='\n')
+                operation = int(input("请输入下一步操作1：修改名片  2：删除名片  3：返回上一级:"))
+                print("*" * 50, end='\n')
+                if operation == 1:
+                    alterUserInfo(stu, stu.index(s), name)
+                elif operation == 2:
+                    deleteOne(stu, stu.index(s))
+                elif operation == 3:
+                    return
     print("未查询到相关数据")
     return
 
@@ -30,7 +36,7 @@ def checkAll(stuList):
     print("以上为所有数据")
 
 
-def add_student():
+def add_student(stu_list):
     """
     add student maps to list
     :return: a student map
@@ -39,33 +45,34 @@ def add_student():
     print("请输入相关数据")
     name = input("姓名：")
     phone = input("电话：")
-    while not re.match("^1[3,4,5,7,8,9]\d{9}$", phone):
-        print("手机号格式有误，请再次输入")
-        phone = input("电话：")
     qq = input("QQ：")
-    while not re.match("[1-9][0-9]{4,10}", qq):
-        print("qq号格式有误，请再次输入")
-        qq = input("qq：")
     email = input("邮件：")
-    return {
+    stu_list.append({
         "name": name,
         "phone": phone,
         "qq": qq,
         "email": email
-    }
+    })
+    print("%s 添加成功" % name)
 
 
-def deleteOne(stu):
+def deleteOne(stu, index):
     """
     delete an item by name
     :return: null
     @author:GXJ
     """
-    name = input("输入要删除学生的姓名:")
-    for s in stu:
-        if s["name"] == name:
-            stu.remove(s)
-            print("删除完成")
-            return
-    print("数据库未录入此人信息")
+    stu.remove(stu[index])
+    print("删除完成")
     return
+
+
+def alterUserInfo(stu_list, index, name):
+    for i in stu_list:
+        if i["name"] == name:
+            i["name"] = input("姓名：")
+            i["phone"] = input("电话：")
+            i["qq"] = input("QQ：")
+            i["email"] = input("邮件：")
+            print("修改成功")
+            break
